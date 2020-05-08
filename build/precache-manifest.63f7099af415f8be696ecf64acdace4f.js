6fa3def6ddea3c92edd35911c14a20d0c2c78b18
Tharshi <you@example.com>
2020-05-08 20:57:35 +0530

    funwork3

---
+++ build/precache-manifest.63f7099af415f8be696ecf64acdace4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0569e0f5d6caf88e34f63187c9b92c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47c0b83db79cd475d857",
+    "url": "/static/css/main.593de5d1.chunk.css"
+  },
+  {
+    "revision": "c301bc5028c7df644770",
+    "url": "/static/js/2.2b752fa2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b752fa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c0b83db79cd475d857",
+    "url": "/static/js/main.b4d56083.chunk.js"
+  },
+  {
+    "revision": "1249babb1f9002060aaf",
+    "url": "/static/js/runtime-main.6a2302b0.js"
+  }
+]);
